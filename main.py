3f6536a07891322af62c92062e89c1b02d2345ee
ABEL ITO <1299927@senati.pe>
2024-04-29 11:10:08 -0500

    Se agrado una calculadora con los operadores basicos.

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ b=5
 print ("hola mundo")
 print ("La suma es:",a+b)
 print ("La resta es:",a-b)
+print("**************************************")
+print("*************CALCULADORA**************")
+print("**************************************")
+num1 = int(input("Introduce el primer numero:"))
+num2 = int(input("Introduce el Segundo numero:"))
+print ("La suma de ",num1,"+",num2,"=",num1+num2)
+print ("La resta de ",num1,"-",num2,"=",num1-num2)
+print ("La division de ",num1,"/",num2,"=",num1/num2)
+print ("La multiplicacion de ",num1,"*",num2,"=",num1*num2)
+
+
+
